559dcfc71900a6919d8068b4896145821b4de27a
Charles McKelvey <chazmckelvey@gmail.com>
2019-10-29 16:50:06 -0400

    Set up admin to have the classes associated and fixed the many to many to be foriegn keys

--- AnswerQuest/admin.py
+++ AnswerQuest/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Question, Answer
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
+admin.site.register(User, UserAdmin, Question, Answer)
