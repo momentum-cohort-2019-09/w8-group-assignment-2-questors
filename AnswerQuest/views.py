aa6f2c4df3675536f3379e05095423ae5accad2a
Charles McKelvey <chazmckelvey@gmail.com>
2019-10-29 14:06:32 -0400

    Bunch of information for our views and the functions associated with our urls.py

--- AnswerQuest/views.py
+++ AnswerQuest/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# These can be subject to change but these were just names I came up with.
+# Remember changing names here means you have to change them in the urls.py as well
+
+
+def question(request):
+    # TODO
+    # 1. Make question adder link to Nav page
+    # 2. Make answer button that is a JavaScript added form field that when submitted is tied and added to the Question object in the model
+    pass
+
+
+def home_page(request):
+    pass
+
+
+def user_profile(request):
+    pass
