b7f867c09aa4c4bf7c7908d98411d9636b9d17e8
mickymck <mickymckeon@gmail.com>
2019-10-29 16:34:52 -0400

    Models updated to remove many to many errors

--- AnswerQuest/models.py
+++ AnswerQuest/models.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 class User(AbstractUser):
     is_registered = models.BooleanField(default=False)
     email = models.EmailField()
-    questions = models.ManyToManyField(to='Question', blank=True, related_name='questors')
-    answers = models.ManyToManyField(to='Answer', blank=True, related_query_name='respondors')
+    questions = models.ForeignKey(to='Question', on_delete=models.SET_NULL, blank=True, null=True, related_name='questors')
+    answers = models.ForeignKey(to='Answer', on_delete=models.SET_NULL, blank=True, null=True, related_query_name='respondors')
 
     # TODO create a form to collect usernames and passwords
 
@@ -21,9 +21,10 @@ class Question(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.SET_NULL, blank=True, null=True, related_name='quests')
     title = models.CharField(max_length=100, blank=True, null=True)
     body = models.TextField(blank=True, null=True)
-    answers = models.ManyToManyField(to='Answer', blank=True, related_query_name='quests')
+    answers = models.ForeignKey(to='Answer', on_delete=models.SET_NULL, blank=True, null=True, related_query_name='quests')
     post_date = models.DateField(default=timezone.now)
     is_solved = models.BooleanField(default=False)
+    # is_starred = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
