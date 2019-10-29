783121984017b5cca558083ba53ad49c5eaef23f
Charles McKelvey <chazmckelvey@gmail.com>
2019-10-29 12:45:59 -0400

    Took away redundant base.html

--- AnswerQuest/models.py
+++ AnswerQuest/models.py
@@ -4,38 +4,49 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class User(AbstractUser):
     is_registered = models.BooleanField(default=False)
     email = models.EmailField()
-    questions = models.ManyToManyField(to='Question', blank=True, related_name='questors')
-    answers = models.ManyToManyField(to='Answer', blank=True, related_query_name='respondors')
+    questions = models.ManyToManyField(
+        to='Question', blank=True, related_name='questors')
+    answers = models.ManyToManyField(
+        to='Answer', blank=True, related_query_name='respondors')
 
     # TODO create a form to collect usernames and passwords
-
+    username = models.CharField(unique=True, blank=True, null=True)
+    password = models.CharField(blank=True, null=True)
     # TODO figure out starred
 
     # TODO figure out how to make this one is_correct specific?
     # solutions = models.ManyToManyField(to='Answer', blank=True, related_query_name='solvors')
+    def __str__(self):
+        return self.username
+
 
 class Question(models.Model):
-    author = models.ForeignKey(to=User, on_delete=models.SET_NULL, blank=True, null=True, related_name='quests')
+    author = models.ForeignKey(
+        to=User, on_delete=models.SET_NULL, blank=True, null=True, related_name='quests')
     title = models.CharField(max_length=100, blank=True, null=True)
     body = models.TextField(blank=True, null=True)
-    answers = models.ManyToManyField(to='Answer', blank=True, related_query_name='quests')
+    answers = models.ManyToManyField(
+        to='Answer', blank=True, related_query_name='quests')
     post_date = models.DateField(default=timezone.now)
     is_solved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
+
 class Answer(models.Model):
-    question = models.ForeignKey(to=Question, on_delete=models.CASCADE, blank=True, null=True, related_name='responses')
-    author = models.ForeignKey(to=User, on_delete=models.SET_NULL, blank=True, null=True,related_name='responses')
+    question = models.ForeignKey(
+        to=Question, on_delete=models.CASCADE, blank=True, null=True, related_name='responses')
+    author = models.ForeignKey(
+        to=User, on_delete=models.SET_NULL, blank=True, null=True, related_name='responses')
     body = models.TextField(blank=True, null=True)
     post_date = models.DateField(default=timezone.now)
     upvotes = models.PositiveIntegerField(default=0)
     downvotes = models.PositiveIntegerField(default=0)
     is_correct = models.BooleanField(default=False)
 
-
-    
+    def __str__(self):
