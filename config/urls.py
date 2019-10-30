8dd127ad236aa66f6a346dcd0aa16524922e3d8c
mickymck <mickymckeon@gmail.com>
2019-10-29 21:49:23 -0400

    added Forms and made the pose_question view, template, and url

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('AnswerQuest/user_profile/<int:pk>',
          views.user_profile, name='user_profile'),
+    path('AnswerQuest/pose_question', views.pose_question, name="pose_question"),
     path('AnswerQuest/question/<int:pk>', views.question, name="question"),
     path('admin/', admin.site.urls),
 ]
