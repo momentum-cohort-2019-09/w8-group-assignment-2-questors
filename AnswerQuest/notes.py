4a8f401421d2f11f2f15ab8daa665528b9a3de11
Kavleer <chriskabler@yahoo.com>
2019-11-03 12:02:25 -0500

    login updates

--- AnswerQuest/notes.py
+++ AnswerQuest/notes.py
@@ -1,3 +1,12 @@
 
 u: mickyMcK
 p: mickypass
+
+def index_view(request):
+    return render(request, "directory/index.html")
+    latest_cohort = Cohort.objects.filter(
+        end_date__lte=timezone.now().date()).order_by('-end_date')[0]
+    return render(request, "directory/index.html", {"cohort": latest_cohort})
+
+    post_date = models.DateField(default=timezone.now)
+    is_solved = models.BooleanField(default=False)
