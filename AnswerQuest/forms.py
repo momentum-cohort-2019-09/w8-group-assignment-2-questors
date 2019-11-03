e8ab7d57b41bb4dfc1be3e89f18f4c5d74da6b35
Kavleer <chriskabler@yahoo.com>
2019-11-03 14:43:09 -0500

    Fix base.html

--- AnswerQuest/forms.py
+++ AnswerQuest/forms.py
@@ -9,7 +9,7 @@ class ProfileForm(forms.ModelForm):
             'username',
             'email',
             'avatar',
-            'starred_answer',
+            'starred_answers',
         ]
 
 
